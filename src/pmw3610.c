da6f3060b62ccfa89e3bdd7c26fa022ae498db05
rain-bbb <perfume.yui.bbb@gmail.com>
2026-01-12 19:55:28 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -455,6 +455,26 @@ static int pmw3610_report_data(const struct device *dev) {
     int16_t y = TOINT16((buf[PMW3610_Y_L_POS] + ((buf[PMW3610_XY_H_POS] & 0x0F) << 8)), 12);
     LOG_DBG("x/y: %d/%d", x, y);
 
+#ifdef CONFIG_PMW3610_ADJUSTABLE_MOUSESPEED
+    int16_t movement_size = abs(x) + abs(y);
+
+    float speed_multiplier = 1.0; //速度の倍率
+    if (movement_size > 60) {
+        speed_multiplier = 3.0;
+    }else if (movement_size > 30) {
+        speed_multiplier = 1.5;
+    }else if (movement_size > 10) {
+        speed_multiplier = 1.0;
+    }else if (movement_size > 5) {
+        speed_multiplier = 0.8;
+    }else if (movement_size > 1) {
+        speed_multiplier = 0.6;
+    }
+
+    x = x * speed_multiplier;
+    y = y * speed_multiplier;
+#endif
+
 #ifdef CONFIG_PMW3610_ALT_SMART_ALGORITHM
     int16_t shutter = ((int16_t)(buf[PMW3610_SHUTTER_H_POS] & 0x01) << 8) 
                     + buf[PMW3610_SHUTTER_L_POS];
